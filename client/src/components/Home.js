d2ba3e59d1baca67091b1199ce9aa817b723fea2
hikio-17 <fajri010217@gmail.com>
2022-07-30 11:02:20 +0700

    add menu items to homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { getNewArrivals } from "./../redux/actionts/filterActions";
+import { getProducts } from "../redux/actionts/productActions";
 import { useDispatch, useSelector } from "react-redux";
 import { showLoading } from "./../helpers/loading";
 import Card from "./Card";
@@ -11,17 +12,23 @@ const Home = () => {
     dispatch(getNewArrivals());
   }, [dispatch]);
 
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
   const { newArrivals } = useSelector((state) => state.filters);
+  const { products } = useSelector((state) => state.products);
   const { loading } = useSelector((state) => state.loading);
 
   return (
     <section className="home-page">
       <div className="banner-image"></div>
       {loading ? (
-        <div>{showLoading()}</div>
+        <div className="text-center">{showLoading()}</div>
       ) : (
         <>
           <div className="container">
+            <hr className="py-3" />
             <h3 className="py-4">New Arrivals</h3>
             <div className="row">
               {newArrivals &&
@@ -31,6 +38,15 @@ const Home = () => {
                     product={newArrival}
                     homePage={true}
                   />
+                ))}
+            </div>
+
+            <hr className="py-3" />
+            <h3 className="py-4">Menu</h3>
+            <div className="row">
+              {products &&
+                products.map((product) => (
+                  <Card key={product._id} product={product} homePage={true} />
                 ))}
             </div>
           </div>
