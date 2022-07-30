d53b2116588332ad5101e71cfd4a60c838fabdfd
hikio-17 <fajri010217@gmail.com>
2022-07-30 17:39:29 +0700

    add fitur search adn filter by category in shop page

--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -1,13 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getProducts } from "../redux/actionts/productActions";
+import { getProductsByFilter } from "../redux/actionts/filterActions";
 import Card from "./Card";
+import { getCategories } from "./../redux/actionts/categoryActions";
+
 const Shop = () => {
+  const [text, setText] = useState("");
+  const [categoryIds, setCategoryIds] = useState([]);
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.products);
+  const { categories } = useSelector((state) => state.categories);
+
+  //event handler
+  const handleSearch = (event) => {
+    resetState();
+    setText(event.target.value);
+    dispatch(getProductsByFilter({ type: "text", query: event.target.value }));
+  };
+
+  const handleCategory = (event) => {
+    resetState();
+    const currentCategoryChecked = event.target.value;
+    const allCategoriesChecked = [...categoryIds];
+    const indexFound = allCategoriesChecked.indexOf(currentCategoryChecked);
+
+    let updatedCategoryIds;
+    if (indexFound === -1) {
+      //add
+      updatedCategoryIds = [...categoryIds, currentCategoryChecked];
+      setCategoryIds(updatedCategoryIds);
+    } else {
+      //remove
+      updatedCategoryIds = [...categoryIds];
+      updatedCategoryIds.splice(indexFound, 1);
+      setCategoryIds(updatedCategoryIds);
+    }
+
+    dispatch(
+      getProductsByFilter({ type: "category", query: updatedCategoryIds })
+    );
+  };
+
+  const resetState = () => {
+    setText("");
+    setCategoryIds([]);
+  };
 
   useEffect(() => {
     dispatch(getProducts());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(getCategories());
   }, [dispatch]);
   return (
     <section className="shop-page m-4">
@@ -15,7 +60,55 @@ const Shop = () => {
         <h1 className="display-4">Shop</h1>
       </div>
       <div className="row">
-        <div className="col-md-3 border-right">Filters go her</div>
+        <div className="col-md-3 border-right">
+          <div className="text-muted mb-2">
+            Filters <span className="fas fa-sliders-h"></span>
+          </div>
+
+          <nav className="navbar navbar-light bg-light justify-content-between border-top p-3">
+            <form className="form-inline my-2 my-lg-0">
+              <input
+                className="form-control col-sm-8 mr-2"
+                type="search"
+                placeholder="Search....."
+                aria-label="Search"
+                name="search"
+                value={text}
+                onChange={handleSearch}
+              />
+              <button
+                className="btn btn-outline-success  my-sm-0 "
+                type="submit"
+                disabled={true}
+              >
+                Search
+              </button>
+            </form>
+          </nav>
+
+          <div className="border-top border-bottom bg-light p-3">
+            {categories &&
+              categories.map((category) => (
+                <div key={category._id} className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    name="category"
+                    value={category._id}
+                    checked={categoryIds.includes(category._id)}
+                    id="flexChecked"
+                    onChange={handleCategory}
+                  />
+                  <label
+                    className="form-check-label"
+                    htmlFor="flexCheckChecked"
+                  >
+                    {category.category}
+                  </label>
+                </div>
+              ))}
+          </div>
+        </div>
         <div className="col-md-9">
           <div className="row">
             {products.map((product) => (
--- controllers/filter.js
+++ controllers/filter.js
@@ -19,3 +19,30 @@ exports.getNewArrivals = async (req, res) => {
     });
   }
 };
+
+exports.getProductsByFilter = async (req, res) => {
+  const { type, query } = req.body;
+  try {
+    let products;
+
+    switch (type) {
+      case "text":
+        products = await Product.find({ $text: { $search: query } });
+        break;
+      case "category":
+        products = await Product.find({ productCategory: query });
+        break;
+    }
+
+    if (!products.length > 0) {
+      products = await Product.find({});
+    }
+
+    res.json({ products });
+  } catch (err) {
+    console.log(err, "filter Controller.getProductsByFilter error");
+    res.status(500).json({
+      errorMessage: "Please try again later",
+    });
+  }
+};
